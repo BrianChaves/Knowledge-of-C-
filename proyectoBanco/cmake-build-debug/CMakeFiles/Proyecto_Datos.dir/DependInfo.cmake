5615b2d406b7b9fe93f711de148adde79413a3f1
Brian Chaves <80367747+BrianChaves@users.noreply.github.com>
2023-05-30 11:19:09 -0600

    Add files via upload

---
+++ proyectoBanco/cmake-build-debug/CMakeFiles/Proyecto_Datos.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/brian/Music/Knowledge of C++/proyectoBanco/ArbolBinario.cpp" "C:/Users/brian/Music/Knowledge of C++/proyectoBanco/cmake-build-debug/CMakeFiles/Proyecto_Datos.dir/ArbolBinario.cpp.obj"
+  "C:/Users/brian/Music/Knowledge of C++/proyectoBanco/Cliente.cpp" "C:/Users/brian/Music/Knowledge of C++/proyectoBanco/cmake-build-debug/CMakeFiles/Proyecto_Datos.dir/Cliente.cpp.obj"
+  "C:/Users/brian/Music/Knowledge of C++/proyectoBanco/HeapColaPrioridad.cpp" "C:/Users/brian/Music/Knowledge of C++/proyectoBanco/cmake-build-debug/CMakeFiles/Proyecto_Datos.dir/HeapColaPrioridad.cpp.obj"
+  "C:/Users/brian/Music/Knowledge of C++/proyectoBanco/ListaDoble.cpp" "C:/Users/brian/Music/Knowledge of C++/proyectoBanco/cmake-build-debug/CMakeFiles/Proyecto_Datos.dir/ListaDoble.cpp.obj"
+  "C:/Users/brian/Music/Knowledge of C++/proyectoBanco/Menu.cpp" "C:/Users/brian/Music/Knowledge of C++/proyectoBanco/cmake-build-debug/CMakeFiles/Proyecto_Datos.dir/Menu.cpp.obj"
+  "C:/Users/brian/Music/Knowledge of C++/proyectoBanco/Nodo.cpp" "C:/Users/brian/Music/Knowledge of C++/proyectoBanco/cmake-build-debug/CMakeFiles/Proyecto_Datos.dir/Nodo.cpp.obj"
+  "C:/Users/brian/Music/Knowledge of C++/proyectoBanco/main.cpp" "C:/Users/brian/Music/Knowledge of C++/proyectoBanco/cmake-build-debug/CMakeFiles/Proyecto_Datos.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ proyectoBanco/cmake-build-debug/CMakeFiles/Proyecto_Datos.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Proyecto_Datos.dir/ArbolBinario.cpp.obj"
+  "CMakeFiles/Proyecto_Datos.dir/Cliente.cpp.obj"
+  "CMakeFiles/Proyecto_Datos.dir/HeapColaPrioridad.cpp.obj"
+  "CMakeFiles/Proyecto_Datos.dir/ListaDoble.cpp.obj"
+  "CMakeFiles/Proyecto_Datos.dir/Menu.cpp.obj"
+  "CMakeFiles/Proyecto_Datos.dir/Nodo.cpp.obj"
+  "CMakeFiles/Proyecto_Datos.dir/main.cpp.obj"
+  "Proyecto_Datos.exe"
+  "Proyecto_Datos.exe.manifest"
+  "Proyecto_Datos.pdb"
+  "libProyecto_Datos.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Proyecto_Datos.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
